06b250caeb0cafc594552feda828836e1918e515
Kathy <kvsippo@charter.net>
2019-12-12 21:02:36 -0600

    Spa-Day finished without Bonus thus far

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -82,6 +82,11 @@ public class SpaDayController {
             }
         }
 
+        model.addAttribute("name", name);
+        model.addAttribute("skintype", skintype);
+        model.addAttribute("manipedi", manipedi);
+        model.addAttribute("appropriateFacials", appropriateFacials);
+
         return "menu";
     }
 }
